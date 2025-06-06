8b4b5778e1823e0dd38d1681c30977314c73063d
Mateo Monroy <monroymateo1@gmail.com>
2025-06-06 16:43:53 -0500

    listo

---
+++ script.js
@@ -0,0 +1,16 @@
+function descargarContacto() {
+    const vcardData = `BEGIN:VCARD
+VERSION:3.0
+FN:Cosméticos NOVAX
+TEL:+57 3003976771
+END:VCARD`;
+
+    const blob = new Blob([vcardData], { type: 'text/vcard' });
+    const url = URL.createObjectURL(blob);
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = 'contacto.vcf';
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+}
